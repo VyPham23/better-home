71b4212ec85f9b75f0ba9fd2678f3e237417bedf
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-12-16 15:21:41 +0700

    update description card

--- src/components/Section Cover/sectioncover.jsx
+++ src/components/Section Cover/sectioncover.jsx
@@ -19,11 +19,18 @@ const SectionCover = ({ text }) => {
                 <div className="rectangle">
                     <div className="card-body">
                         <div class="row">
-                            <div class="col-md-12 col-lg-3 col-xl-3">
+                            <div class="col-md-12 col-lg-2 col-xl-2 prop-stat">
+                                <span className="stat">123 <br/> Properties Available </span>
                             </div>
-                            <div class="col-md-0 col-lg-6 col-xl-6">
+                            <div class="col-md-0 col-lg-7 col-xl-7 prop-des">
+                                <span className="des">Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States. 
+                                    Its population was 19,728 at the 2020 U.S. Census. 
+                                    The city is located in the South Bay region of the Greater Los Angeles area; it is one of the three Beach Cities.</span>
                             </div>
-                            <div class="col-md-12 col-lg-3 col-xl-3">
+                            <div class="col-md-12 col-lg-3 col-xl-3 prop-all">
+                                <a href="/">
+                                    <button className="btn" id="all-btn">VIEW ALL</button>
+                                </a>
                             </div>
                         </div>
                     </div>
