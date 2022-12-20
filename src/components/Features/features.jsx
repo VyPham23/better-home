42e41befad887baea4f48156b33e6c15f5da18f7
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-12-20 23:20:32 +0700

    commit

--- src/components/Features/features.jsx
+++ src/components/Features/features.jsx
@@ -4,27 +4,8 @@ import './features.css';
 const Features = () => {
     return (
         <div>
-            <div className="container mt-5 mb-5">
-                <div className="dt-body">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-2 col-xl-2 dt-price">
-                            <span className="stat">For sale <br/> $1,500,000 </span>
-                        </div>
-                        <div class="col-md-12 col-lg-6 col-xl-6 dt-des">
-                            <span className="des">Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States. 
-                                Its population was 19,728 at the 2020 U.S. Census. 
-                                The city is located in the South Bay region of the Greater Los Angeles area; it is one of the three Beach Cities.</span>
-                        </div>
-                        <div class="col-md-12 col-lg-4 col-xl-4 dt-all">
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container mt-5 mb-5">
-
+            <div className="ft-container">
                 <div className="fd-item fd-features">
-                    
                     <div className="row">
                         <div className="col-lg-4">
                             <i className="fa fa-check"></i>
--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -4,6 +4,7 @@ import Features from '../../components/Features/features';
 import Navbar from '../../components/Header/Navbar/navbar';
 import UpperNav from '../../components/Header/Navbar/uppernav';
 import Footer from "../../components/Footer/footer";
+import DetailDes from '../../components/Features/detaildes';
 
 const Detail = () => {
     return (
@@ -11,6 +12,7 @@ const Detail = () => {
             <UpperNav/>
             <Navbar/>
             <CarouselFadeExample/>
+            <DetailDes/>
             <Features/>
             <Footer/>
         </div>
