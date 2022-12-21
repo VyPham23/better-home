9eeea7f0944471862a216bf9d4c0d062965d9773
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-12-21 15:46:09 +0700

    black section and price updated

---
+++ src/components/Blackbox/blacksection.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './blacksection.css';
+
+const BlackSection = () => {
+    return (
+        <div className="black">
+            {/* <div className="row"> */}
+            <div className="agn">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div className="txt-area">
+                    <h4>Our agent</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
+                </div>
+            </div>
+            <div className="cpn">
+            <div>
+                    <img src="" alt="" />
+                </div>
+                <div className="txt-area">
+                    <h4>Our agent</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
+                </div>
+            </div>
+            {/* </div> */}
+        </div>
+    );
+};
+
+export default BlackSection;
