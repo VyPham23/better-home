9c53b624d0ba9fe46a6dbdf20520aac90a749b9a
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-12-24 10:48:39 +0700

    fix

--- src/components/Blackbox/blacksection.jsx
+++ src/components/Blackbox/blacksection.jsx
@@ -6,26 +6,27 @@ import Pic2 from './47.png';
 const BlackSection = () => {
     return (
         <div className="black">
-            {/* <div className="row"> */}
-            <div className="agn">
-                <div className="black-pic">
-                    <img src={Pic1} alt="pic" />
+            <div className="row">
+                <div className="col-lg-6 agn">
+                {/* <div className="agn"> */}
+                    <div className="black-pic">
+                        <img src={Pic1} alt="pic" />
+                    </div>
+                    <div className="txt-area">
+                        <h4>Our Agent</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
+                    </div>
                 </div>
-                <div className="txt-area">
-                    <h4>Our Agent</h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
+                <div className="col-lg-6 cpn">
+                    <div className="black-pic">
+                        <img src={Pic2} alt="pic" />
+                    </div>
+                    <div className="txt-area">
+                        <h4>Our Company</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
+                    </div>
                 </div>
             </div>
-            <div className="cpn">
-                <div className="black-pic">
-                    <img src={Pic2} alt="pic" />
-                </div>
-                <div className="txt-area">
-                    <h4>Our Company</h4>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, labore.</p>
-                </div>
-            </div>
-            {/* </div> */}
         </div>
     );
 };
--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -3,12 +3,14 @@ import './intro.css';
 
 function Intro(props) {
     return (
-        <div className="intro_container">
-            <h2 id="title">Real Estate for Business</h2>
-            <div className="intro_button">
-                <button className="navbutton">Shop House or <br/>Business Location</button>
-                <button className="navbutton">Office for Rent</button>
-                <button className="navbutton">Co-working Space</button>
+        <div className="intro">
+            <div className="intro_container">
+                <h2 id="title">Real Estate for Business</h2>
+                <div className="intro_button">
+                    <button className="navbutton">Shop House or <br/>Business Location</button>
+                    <button className="navbutton">Office for Rent</button>
+                    <button className="navbutton">Co-working Space</button>
+                </div>
             </div>
         </div>
     );
