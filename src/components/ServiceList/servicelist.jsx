394565984f1bd45162be568c866de672305d2ebf
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-12-28 06:06:36 +0700

    Feat: edit UI for service list in home page

--- src/components/ServiceList/servicelist.jsx
+++ src/components/ServiceList/servicelist.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './service.css';
 import ServiceItem from './serviceitem';
+import pic1 from './pic1.jpg';
+import pic2 from './pic2.jpg';
+import pic3 from './pic3.jpg';
+import pic4 from './pic4.jpg';
 
 const ServiceList = () => {
     return (
@@ -8,10 +12,10 @@ const ServiceList = () => {
             <div className="container">
                 {/* <Title title={title.text} description={title.description} /> */}
                 <div className='row'>
-                    <ServiceItem name="SALE YOUR HOUSE" />
-                    <ServiceItem name="RENT OUT YOUR HOUSE" />
-                    <ServiceItem name="APARTMENT MANAGEMENT SERVICE" />
-                    <ServiceItem name="INTERIOR DESIGN" />
+                    <ServiceItem name="SALE YOUR HOUSE" picture={pic1} />
+                    <ServiceItem name="RENT OUT YOUR HOUSE" picture={pic2} />
+                    <ServiceItem name="APARTMENT MANAGEMENT SERVICE" picture={pic3} />
+                    <ServiceItem name="INTERIOR DESIGN" picture={pic4} />
                 </div>
             </div>
         </section>
