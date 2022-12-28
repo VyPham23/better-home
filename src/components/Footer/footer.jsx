833b88c0373f3cf536fbad1d096df528f42a77cd
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-12-29 00:34:21 +0700

    Feat: fix reponsive for footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,60 +4,69 @@ import './footer.css';
 const Footer = () => {
     return (
         <section className="footer">
-            
-            <ContactCover/>
+
+            <ContactCover />
 
             <div className="container">
                 <div className="row">
 
-                    <div className="block-infor col-lg-4 col-md-6">
+                    <div className="block-infor col-lg-4 col-md-6 col-6">
                         <div className="footer-title d-flex">
-                            <i className="fas fa-home"></i> <p> &nbsp; Better Home</p>
+                            <i className="fas fa-home"></i><p>&nbsp; Better Home</p>
                         </div>
-                        
+
                         <ul className="footer-ul">
                             <li className="d-flex">
-                                <div className="footer-info-item"><i className="fas fa-map-marker-alt"></i></div> <span>
-                                Room 302, Saigon Pearl Office Building,<br/>
-                                92C Nguyen Huu Canh St, Binh Thanh District, <br/>
-                                Ho Chi Minh City, VietNam
+                                <div className="footer-info-item">
+                                    <i className="fas fa-map-marker-alt"></i>
+                                </div>
+                                <span className='infor-company'>&nbsp;
+                                    Room 302, Saigon Pearl Office Building,
+                                    92C Nguyen Huu Canh St, Binh Thanh District,
+                                    Ho Chi Minh City, VietNam
                                 </span>
                             </li>
 
                             <li className="d-flex">
-                                <div className="footer-info-item" ><i className="fas fa-envelope"></i></div> <span>betterhomesinvest@gmail.com</span>
+                                <div className="footer-info-item" >
+                                    <i className="fas fa-envelope"></i>
+                                </div>
+                                <span className='infor-company'>&nbsp;betterhomesinvest@gmail.com</span>
                             </li>
-                            
+
                             <li className="d-flex">
-                                <div className="footer-info-item"><i className="fas fa-phone-alt"></i></div> <span>(+84) 090. 453.234</span>
+                                <div className="footer-info-item">
+                                    <i className="fas fa-phone-alt"></i>
+                                </div>
+                                <span className='infor-company'>&nbsp;(+84) 090. 453.234</span>
                             </li>
                         </ul>
-                        
+
                     </div>
-                    <div className="block-infor col-lg-3 col-md-6">
+                    <div className="block-infor col-lg-3 col-md-6 col-6">
                         <p className="footer-title d-flex">Menu</p>
                         <ul className="footer-ul">
-                            <li className="d-flex"><a href='/buy_a_house'>Buy A House</a></li>
-                            <li className="d-flex"><a href='/rent_a_house'>Rent A House</a></li>
-                            <li className="d-flex"><a href='/service_apartment'>Service Apartment</a></li>
-                            <li className="d-flex"><a href='/servicefor_HouseOwner'>Service for House Owner</a></li>
-                            <li className="d-flex"><a href='/property'>Real Estate fo Business</a></li>
+                            <li className="d-flex infor-company"><a href='/buy_a_house'>Buy A House</a></li>
+                            <li className="d-flex infor-company"><a href='/rent_a_house'>Rent A House</a></li>
+                            <li className="d-flex infor-company"><a href='/service_apartment'>Service Apartment</a></li>
+                            <li className="d-flex infor-company"><a href='/servicefor_HouseOwner'>Service for House Owner</a></li>
+                            <li className="d-flex infor-company"><a href='/property'>Real Estate fo Business</a></li>
                         </ul>
                     </div>
-                    <div className="block-infor col-lg-3 col-md-6">
+                    <div className="block-infor col-lg-3 col-md-6 col-6">
                         <div>
                             <p className="footer-title d-flex">Our Company</p>
                             <ul className="footer-ul">
-                                <li className="d-flex"><a href='/contact'>Contact</a></li>
-                                <li className="d-flex"><a href='/about'>About Us</a></li>
-                                <li className="d-flex"><a href='/news'>News</a></li>
+                                <li className="d-flex infor-company"><a href='/contact'>Contact</a></li>
+                                <li className="d-flex infor-company"><a href='/about'>About Us</a></li>
+                                <li className="d-flex infor-company"><a href='/news'>News</a></li>
                             </ul>
 
-                            
+
                         </div>
                     </div>
 
-                    <div className="block-infor col-lg-2 col-md-6">
+                    <div className="block-infor col-lg-2 col-md-6 col-6">
                         <p className="footer-title d-flex">Social Media</p>
                         <div className="footer-social">
                             <div className="footer-social-item fbicon"><i className="fab fa-facebook-f"></i></div>
@@ -66,7 +75,7 @@ const Footer = () => {
                         </div>
                     </div>
 
-                   
+
 
                 </div>
             </div>
