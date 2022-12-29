38a3b86b1ceaa18afa69f884a74836fefe87c2b8
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-12-29 13:27:34 +0700

    udate detail page

--- src/components/Features/detaildes.jsx
+++ src/components/Features/detaildes.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import BedImg from './100-bed.png';
+import BathImg from './102-bath.png';
+import SqftImg from './101-sqft.png'
 
 const DetailDes = () => {
     return (
@@ -23,29 +26,59 @@ const DetailDes = () => {
             <div className="dt-body">
                 <div class="row">
                     <div class="col-md-12 col-lg-3 col-xl-3 dt-price">
-                        <span className="stat">For sale <br/> $1,500,000 </span>
+                        <h4>For sale</h4> <br/> <p>$1,500,000</p>
                     </div>
                     <div class="col-md-12 col-lg-6 col-xl-6 dt-des">
                         <div class="row">
-                            <div className="info">
-
-                            </div>
+                            <div className="info"><h3>7983 Valmont Rd, Boulder, CO 80301</h3></div>
                         </div>                            
-                        <span className="des">Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States. 
+                        <div class="row">
+                            <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
+                                <img src={BedImg} alt=""/>
+                                <span>Bed 4</span>
+                            </div>
+                            <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
+                                <img src={BathImg} alt=""/>
+                                <span>Bath 4</span>
+                            </div>
+                            <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
+                                <img src={SqftImg} alt=""/>
+                                <span>Sqft 3122</span>
+                            </div>
+                        </div>
+                        <div className="description">
+                        <span>Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States. 
                             Its population was 19,728 at the 2020 U.S. Census. 
                             The city is located in the South Bay region of the Greater Los Angeles area; it is one of the three Beach Cities.</span>
+                        </div>
+                        
                     </div>
-                    <div class="col-md-12 col-lg-3 col-xl-3 dt-all">
+                    <div class="col-md-12 col-lg-3 col-xl-3 dt-moreinfo">
                         <div class="row">
-                            <div className="col-lg-6 type">Type</div>
-                            <div className="col-lg-6 park">Park</div>
+                            <div className="col-lg-6 type">
+                                <div><h6>Type</h6></div>
+                                <div><p>Single Family</p></div>
+                            </div>
+                            <div className="col-lg-6 type">
+                                <div><h6>Park</h6></div>
+                                <div><p>2 Spaces</p></div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div className="col-lg-6 built">Built</div>
-                            <div className="col-lg-6 lot">Lot</div>
+                            <div className="col-lg-6 type">
+                                <div><h6>Built in</h6></div>
+                                <div><p>2010</p></div>
+                            </div>
+                            <div className="col-lg-6 type">
+                                <div><h6>Lot</h6></div>
+                                <div><p>1.36 Acress</p></div>
+                            </div>
                         </div>
                         <div class="row">
-                            <div className="col-lg-6 heating">Heating</div>
+                            <div className="col-lg-6 type">
+                                <div><h6>Heating</h6></div>
+                                <div><p>Ari Cooling</p></div>
+                            </div>
                             <div></div>
                         </div>
                     </div>
