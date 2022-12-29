17c1232d8193d903ab458c246ce8346903a92051
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-12-30 00:32:14 +0700

    Feat: done front end for details top in detail page

--- src/components/Features/detaildes.jsx
+++ src/components/Features/detaildes.jsx
@@ -1,57 +1,139 @@
-import React from 'react';
 import BedImg from './100-bed.png';
 import BathImg from './102-bath.png';
 import SqftImg from './101-sqft.png'
+import Calendar from './calendar.png'
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
+import apartment from './product1.jpeg';
+import './features.css';
 
 const DetailDes = () => {
     return (
-        <div className="des-container">
-            {/* <div className="dt-body-0">
-            </div> */}
-            <div className="dt-body upper">
-                <div class="row up-part">
-                    <div class="col-md-0 col-lg-3 col-xl-3 left-side">
-                        <span className="left-text">Photos</span>
+        <div className='details_apartment_area'>
+            <div className='slider_area'>
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 cr-img img-fluid"
+                            src={apartment}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+                <div class="row in_slider_area">
+                    <div className='col-lg-4 col-md-4 col-4 in_slider_display in_slider_display_photo'>
+                        <span>Photos</span>
+                        <span className='button_pagination_area'>
+                            <button className='button_pagination'>&lt;</button>
+                            01 / 09
+                            <button className='button_pagination'>&gt;</button>
+                        </span>
                     </div>
-                    <div class="col-md-0 col-lg-6 col-xl-6 middle-part">
+
+                    <div className='col-lg-4 col-md-4 col-4 not_display'>
                     </div>
-                    <div class="col-md-12 col-lg-3 col-xl-3 right-side">
-                        <div className="txt-ctn">
-                            <span className="left-text">Schedule A Visit</span>
-                        </div>
+
+                    <div className='col-lg-4 col-md-4 col-4 in_slider_display'>
+                        <img className='icon_details' src={Calendar} alt="item" />
+                        <span>Schedule A Visit</span>
                     </div>
                 </div>
             </div>
-            
-            <div className="dt-body">
+
+            <div className="row apart_detail_area">
+                <div className="apart_detail_area_left col-lg-3 col-md-4 col-4">
+                    <div className="apart_detail_area_left_block">
+                        <span>FOR SALE</span>
+                        <p>$1,500,000</p>
+                    </div>
+                </div>
+                <div className="apart_detail_area_middle col-lg-6 col-md-4 col-4">
+                    <div className='apart_name' style={{ textAlign: "justify" }}>
+                        <span>
+                            7983 Valmont Rd, Boulder, CO 80301
+                        </span>
+                    </div>
+                    <div className='apart_detail_icon_infor'>
+                        <span className='title_icon'>
+                            <img className='icon_details' src={BedImg} alt="detail_icon" /> BEDS 
+                            &nbsp; <span className='number'>4</span>
+                        </span>
+                        <span className='title_icon'>
+                            <img className='icon_details' src={BathImg} alt="detail_icon" /> BATHS 
+                            &nbsp; <span className='number'>4</span>
+                        </span>
+                        <span className='title_icon'>
+                            <img className='icon_details' src={SqftImg} alt="detail_icon" /> SQFT 
+                            &nbsp; <span className='number'>3.122</span>
+                        </span>
+                    </div>
+                    <div style={{ textAlign: "justify" }}>
+                        <span >
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Aliquam quia iste possimus repudiandae provident quasi,
+                            eveniet amet culpa aspernatur dolores facere voluptate excepturi
+                            modi? Repellendus earum aliquam praesentium ab accusamus.
+                        </span>
+                    </div>
+                </div>
+                <div className="apart_detail_area_right col-lg-3 col-md-4 col-4">
+                    <div style={{ textAlign: "justify" }} className="row apart_detail_area_right_1">
+                        <div>
+                            <span>TYPE</span>
+                            <p>Single Family</p>
+                        </div>
+                        <div>
+                            <span>PARKING</span>
+                            <p>2 Spaces</p>
+                        </div>
+                    </div>
+                    <div style={{ textAlign: "justify" }} className="row apart_detail_area_right_2">
+                        <div>
+                            <span>YEAR BUILT</span>
+                            <p>2007</p>
+                        </div>
+                        <div>
+                            <span>LOT</span>
+                            <p>1.36 Acress</p>
+                        </div>
+                    </div>
+                    <div className="row apart_detail_area_right_3" style={{ textAlign: "justify" }}>
+                        <span>HEATING</span>
+                        <p>Air Cooling</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className="dt-body">
                 <div class="row">
                     <div class="col-md-12 col-lg-3 col-xl-3 dt-price">
-                        <h4>For sale</h4> <br/> <p>$1,500,000</p>
+                        <h4>For sale</h4> <br /> <p>$1,500,000</p>
                     </div>
                     <div class="col-md-12 col-lg-6 col-xl-6 dt-des">
                         <div class="row">
                             <div className="info"><h3>7983 Valmont Rd, Boulder, CO 80301</h3></div>
-                        </div>                            
+                        </div>
                         <div class="row">
                             <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
-                                <img src={BedImg} alt=""/>
+                                <img src={BedImg} alt="" />
                                 <span>Bed 4</span>
                             </div>
                             <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
-                                <img src={BathImg} alt=""/>
+                                <img src={BathImg} alt="" />
                                 <span>Bath 4</span>
                             </div>
                             <div className="col-md-4 col-lg-4 col-xl-4 basic-figure">
-                                <img src={SqftImg} alt=""/>
+                                <img src={SqftImg} alt="" />
                                 <span>Sqft 3122</span>
                             </div>
                         </div>
                         <div className="description">
-                        <span>Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States. 
-                            Its population was 19,728 at the 2020 U.S. Census. 
-                            The city is located in the South Bay region of the Greater Los Angeles area; it is one of the three Beach Cities.</span>
+                            <span>Hermosa Beach is a beachfront city in Los Angeles County in the U.S. state of California, United States.
+                                Its population was 19,728 at the 2020 U.S. Census.
+                                The city is located in the South Bay region of the Greater Los Angeles area; it is one of the three Beach Cities.</span>
                         </div>
-                        
+
                     </div>
                     <div class="col-md-12 col-lg-3 col-xl-3 dt-moreinfo">
                         <div class="row">
@@ -83,7 +165,82 @@ const DetailDes = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
+
+            {/* <div className="ft-container">
+                <div className="fd-item fd-features">
+                    <div className="row ft-row">
+                        <div className="col-lg-4">
+                            <i className="fa fa-check"></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                    </div>
+                    <div className="row ft-row">
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check"></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                    </div>
+                    <div className="row ft-row">
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                    </div>
+                    <div className="row ft-row">
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                    </div>
+                    <div className="row ft-row">
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                        <div className="col-lg-4">
+                            <i className="fa fa-check" ></i>
+                            <span>Lorem Ipsum</span>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
         </div>
     );
 };
--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import CarouselFadeExample from '../../components/Carousel/Carousel';
-import Features from '../../components/Features/features';
 import Navbar from '../../components/Header/Navbar/navbar';
 import UpperNav from '../../components/Header/Navbar/uppernav';
 import Footer from "../../components/Footer/footer";
@@ -8,12 +5,10 @@ import DetailDes from '../../components/Features/detaildes';
 
 const Detail = () => {
     return (
-        <div>
+        <div style={{ overflowX: "hidden" }}>
             <UpperNav/>
             <Navbar/>
-            <CarouselFadeExample/>
             <DetailDes/>
-            <Features/>
             <Footer/>
         </div>
     );
