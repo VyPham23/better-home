2b03394cea6cb3d941f0a304dc214dfc2d59fb7c
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-12-31 14:41:45 +0700

    Feat: fetch api for news detail

--- src/components/News/News_Details.jsx
+++ src/components/News/News_Details.jsx
@@ -1,11 +1,65 @@
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
 const News_Details = () => {
-  return (
-    <div>
-      <section className="section-all-re">
-            <div className="container">
+
+    const { id } = useParams()
+    const url_news_by_id = "http://localhost/admin_api/public/api/v1/newspage/" + id;
+
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [date, setDate] = useState("")
+    const [description, setDescription] = useState(<h2>ABC</h2>)
+
+    /*get news by id*/
+    useEffect(() => {
+        const getNews = async () => {
+            const newsFromServer = await fetchNews()
+            setTitle(newsFromServer["title_news"])
+            setAuthor(newsFromServer["author_news"])
+            setDate(newsFromServer["created_at"])
+            setDescription(newsFromServer["description_news"].replaceAll('"', ''))
+        }
+        getNews()
+    }, [])
+
+    const fetchNews = async () => {
+        const res = await fetch(url_news_by_id)
+        const data = await res.json()
+        return data['data'][0]
+    }
+
+    const getDate = (date) =>{
+        const getDate = date.substring(0, 10)
+  
+        const dateArray = getDate.split('-')
+  
+        const formatedDate = dateArray[2] + '-' + dateArray[1] + '-' + dateArray[0]
+  
+        return formatedDate
+    }
+
+    /*----------------*/
+
+    return (
+        <div>
+            <section className="intro_news mt-5">
+                <div className='news_details_title'>
+                    {title}
+                </div>
+                <div className='news_details_sub_infor'>
+                    <span>{author}</span>
+                    <span>{date ? getDate(date) : ""}</span>
+                </div>
+                <article>
+                    {description}
+                </article>
+            </section>
+            <section className="section-all-re">
+                {/* <div className="container">
                 <div className="row">
                     <div className="col-lg-12 col-12 col-md-12 news-start">
-                        <h2><a href="/">Recent News</a></h2>
+                        <h2>Recent News</h2>
                     </div>
                    
                         <div className="col-lg-6 col-12 col-md-12">
@@ -25,13 +79,12 @@ const News_Details = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div> {/* end col */}
-
-                </div>{/* end row */}
-            </div>
-        </section>
-    </div>
-  )
+                        </div>
+                </div>
+            </div> */}
+            </section>
+        </div>
+    )
 }
 
 export default News_Details
