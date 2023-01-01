45ba35909f34a6d0c56d13a834e75e27288462c0
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-01-01 19:44:41 +0700

    Feat: completing fetch apart infor and feature list in apart details

--- src/components/Features/detaildes.jsx
+++ src/components/Features/detaildes.jsx
@@ -1,37 +1,85 @@
 import BedImg from './100-bed.png';
 import BathImg from './102-bath.png';
 import SqftImg from './101-sqft.png'
-import Calendar from './calendar.png'
-
-/*ICON AREA*/
-import internet from './Icon_Features/internet.png'
-import television from './Icon_Features/television.png'
-import telephone from './Icon_Features/phone.png'
-
-import refrigerator from './Icon_Features/refrigerator.png'
-import air_condition from './Icon_Features/air_condition.png'
-import water from './Icon_Features/water.png'
-
-import balcony from './Icon_Features/balcony.png'
-import power from './Icon_Features/power.png'
-import security from './Icon_Features/security.png'
-
-import parking from './Icon_Features/parking.png'
-import store from './Icon_Features/store.png'
-import garden from './Icon_Features/garden.png'
-
-import pool from './Icon_Features/pool.png'
-import furnishture from './Icon_Features/house.png'
-import gym from './Icon_Features/gym.png'
+import House from './house.png'
 
 /*---------*/
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
 import apartment from './product1.jpeg';
 import './features.css';
 
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
 const DetailDes = () => {
+
+    const { id } = useParams()
+    const url_apart_by_id = "http://localhost/admin_api/public/api/v1/details/" + id
+
+    /*all property of apart for rent*/
+    const [projectName, setProjectName] = useState("")
+    const [apartCode, setApartCode] = useState("")
+    const [purpose, setPurpose] = useState("")
+    const [price, setPrice] = useState("")
+    const [address, setAddress] = useState("")
+    const [description, setDescription] = useState("")
+
+    const [numBed, setNumBed] = useState("")
+    const [numPath, setNumPath] = useState("")
+    const [sqft, setSQFT] = useState("")
+    const [type, setType] = useState("")
+    const [yearBuild, setYearBuild] = useState("")
+    const [parking, setParking] = useState("")
+    const [heating, setHeating] = useState("")
+    const [lot, setLot] = useState("")
+
+    const [featureList, setFeatureList] = useState([])
+
+    /*------------------------------*/
+
+    /*get apartment for rent by id*/
+    useEffect(() => {
+        const getApartForRentByID = async () => {
+            const apartForRentFromServer = await fetchApartForRent()
+            setProjectName(apartForRentFromServer["apart_infor"][0]["project_name"])
+            setApartCode(apartForRentFromServer["apart_infor"][0]["apartment_code"])
+            setPurpose(apartForRentFromServer["apart_infor"][0]["purpose"])
+            setPrice(numberWithCommas(apartForRentFromServer["apart_infor"][0]["price"]))
+            setAddress(apartForRentFromServer["apart_infor"][0]["address"])
+            setDescription(apartForRentFromServer["apart_infor"][0]["description"])
+
+            setNumBed(apartForRentFromServer["apart_infor"][0]['num_beds'])
+            setNumPath(apartForRentFromServer["apart_infor"][0]['num_baths'])
+            setSQFT(apartForRentFromServer["apart_infor"][0]['sqft'])
+            setType(apartForRentFromServer["apart_infor"][0]['type'])
+            setYearBuild(apartForRentFromServer["apart_infor"][0]['year_built'])
+            setParking(apartForRentFromServer["apart_infor"][0]['parking'])
+            setHeating(apartForRentFromServer["apart_infor"][0]['heating'])
+            setLot(apartForRentFromServer["apart_infor"][0]['lot'])
+
+            setFeatureList(apartForRentFromServer["features_infor"])
+        }
+        getApartForRentByID()
+    }, [])
+
+    const fetchApartForRent = async () => {
+        const res = await fetch(url_apart_by_id)
+        const data = await res.json()
+        return data['data']
+    }
+    /*--------------*/
+
+    /*formating data*/
+    function numberWithCommas(price) {
+        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    function getIcon(icon) {
+        return icon.toLowerCase().replaceAll('/', '_').replaceAll(' ', '_')
+    }
+    /*------------------*/
+
     return (
         <div className='details_apartment_area pb-5'>
             <div className='slider_area'>
@@ -45,6 +93,12 @@ const DetailDes = () => {
                     </Carousel.Item>
                 </Carousel>
                 <div class="row in_slider_area">
+                    <div className='col-lg-4 col-md-12 col-12 in_slider_display'>
+                        <img className='icon_details' src={House} alt="item" />
+                        <span>{apartCode}</span>
+                    </div>
+                    <div className='col-lg-4 col-md-12 col-12 not_display'>
+                    </div>
                     <div className='col-lg-4 col-md-12 col-12 in_slider_display in_slider_display_photo'>
                         <span>Photos</span>
                         <span className='button_pagination_area'>
@@ -52,14 +106,6 @@ const DetailDes = () => {
                             01 / 09
                             <button className='button_pagination'>&gt;</button>
                         </span>
-                    </div>
-
-                    <div className='col-lg-4 col-md-12 col-12 not_display'>
-                    </div>
-
-                    <div className='col-lg-4 col-md-12 col-12 in_slider_display'>
-                        <img className='icon_details' src={Calendar} alt="item" />
-                        <span>Schedule A Visit</span>
                     </div>
                 </div>
             </div>
@@ -77,44 +123,45 @@ const DetailDes = () => {
                 </div>
 
                 <div className='col-lg-4 col-md-12 col-12 in_slider_display'>
-                    <img className='icon_details' src={Calendar} alt="item" />
-                    <span>Schedule A Visit</span>
+                    <img className='icon_details' src={House} alt="item" />
+                    <span>{apartCode}</span>
                 </div>
             </div>
 
             <div className="row apart_detail_area">
                 <div className="apart_detail_area_left col-lg-3 col-md-12 col-12">
                     <div className="apart_detail_area_left_block">
-                        <span>FOR SALE</span>
-                        <p>$1,500,000</p>
+                        <span>
+                            {purpose === 'for_rent' ? "FOR RENT" : <>
+                                {purpose === 'for_sell' ? "FOR SELL" : "SERVICE APARTMENT"}
+                            </>}
+                        </span>
+                        <p>{price} VND</p>
                     </div>
                 </div>
                 <div className="apart_detail_area_middle col-lg-6 col-md-12 col-12">
                     <div className='apart_name' style={{ textAlign: "justify" }}>
                         <span>
-                            7983 Valmont Rd, Boulder, CO 80301
+                            {projectName}, {address}
                         </span>
                     </div>
                     <div className='apart_detail_icon_infor'>
                         <span className='title_icon'>
                             <img className='icon_details' src={BedImg} alt="detail_icon" /> BEDS
-                            &nbsp; <span className='number'>4</span>
+                            &nbsp; <span className='number'>{numBed}</span>
                         </span>
                         <span className='title_icon'>
                             <img className='icon_details' src={BathImg} alt="detail_icon" /> BATHS
-                            &nbsp; <span className='number'>4</span>
+                            &nbsp; <span className='number'>{numPath}</span>
                         </span>
                         <span className='title_icon'>
                             <img className='icon_details' src={SqftImg} alt="detail_icon" /> SQFT
-                            &nbsp; <span className='number'>3.122</span>
+                            &nbsp; <span className='number'>{sqft}</span>
                         </span>
                     </div>
                     <div style={{ textAlign: "justify" }}>
                         <span className='apart_description' >
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aliquam quia iste possimus repudiandae provident quasi,
-                            eveniet amet culpa aspernatur dolores facere voluptate excepturi
-                            modi? Repellendus earum aliquam praesentium ab accusamus.
+                            {description}
                         </span>
                     </div>
                 </div>
@@ -122,27 +169,27 @@ const DetailDes = () => {
                     <div style={{ textAlign: "justify" }} className="row apart_detail_area_right_1">
                         <div>
                             <span>TYPE</span>
-                            <p>Single Family</p>
+                            <p>{type}</p>
                         </div>
                         <div>
                             <span>PARKING</span>
-                            <p>2 Spaces</p>
+                            <p>{parking}</p>
                         </div>
                     </div>
                     <div style={{ textAlign: "justify" }} className="row apart_detail_area_right_2">
                         <div>
                             <span>YEAR BUILT</span>
-                            <p>2007</p>
+                            <p>{yearBuild}</p>
                         </div>
                         <div>
                             <span>LOT</span>
-                            <p>1.36 Acress</p>
+                            <p>{lot}</p>
                         </div>
                     </div>
                     <div className="row apart_detail_area_right_3" style={{ textAlign: "justify" }}>
                         <div>
                             <span>HEATING</span>
-                            <p>Air Cooling</p>
+                            <p>{heating}</p>
                         </div>
                     </div>
                 </div>
@@ -153,16 +200,19 @@ const DetailDes = () => {
                     <span>FEATURES</span>
                 </div>
                 <div className="row features_item pb-4">
-                    <div className='col-4 col-lg-4 col-md-4'>
-                        <img className='icon_details' src={internet} alt="feature-item" />
-                        &nbsp; <span className='feature_item_name'>Internet</span>
-                    </div>
-                    <div className='col-4 col-lg-4 col-md-4'>
-                        <img className='icon_details' src={television} alt="feature-item" />
+                    {featureList.map((feature) => (
+                        <div className='col-4 col-lg-4 col-md-4'>
+                            <img className='icon_details' src={`/images/Icon_Features/${getIcon(feature.feature_name)}.png`} alt="feature-item" />
+                            &nbsp; <span className='feature_item_name'>{feature.feature_name}</span>
+                        </div>
+                    ))}
+
+                    {/* <div className='col-4 col-lg-4 col-md-4'>
+                        <img className='icon_details' src={Cable_Television} alt="feature-item" />
                         &nbsp; <span className='feature_item_name'>Cable Television</span>
                     </div>
                     <div className='col-4 col-lg-4 col-md-4'>
-                        <img className='icon_details' src={telephone} alt="feature-item" />
+                        <img className='icon_details' src={Telephone} alt="feature-item" />
                         &nbsp; <span className='feature_item_name'>Telephone</span>
                     </div>
 
@@ -193,7 +243,7 @@ const DetailDes = () => {
                     </div>
 
                     <div className='col-4 col-lg-4 col-md-4'>
-                        <img className='icon_details' src={parking} alt="feature-item" />
+                        <img className='icon_details' src={parkingSpace} alt="feature-item" />
                         &nbsp; <span className='feature_item_name'>Parking</span>
                     </div>
                     <div className='col-4 col-lg-4 col-md-4'>
@@ -216,7 +266,7 @@ const DetailDes = () => {
                     <div className='col-4 col-lg-4 col-md-4'>
                         <img className='icon_details' src={gym} alt="feature-item" />
                         &nbsp; <span className='feature_item_name'>GYM</span>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
