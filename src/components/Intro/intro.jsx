dd87b5ad966f8935710077e078563eb4dd94ebbf
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-01-08 03:35:33 +0700

    Feat: redo intro in home page

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -2,14 +2,14 @@ import './intro.css';
 
 function Intro() {
     return (
-        <div className="intro">
+        <div className="intro mt-5">
             <div className="intro_container">
-                <h2 id="title" className='mb-5'>Real Estate for Business</h2>
+                <div><h2 id="title" className='mb-5'>Real Estate for Business</h2></div>
                 <div className="intro_button">
                     <button className="navbutton">
-                        <a className='button_nav_home' href="/real_este">Shop House or <br/>Business Location</a>
+                        <a className='button_nav_home' href="/real_este">Shop House / <br/>Business Location</a>
                     </button>
-                    <button className="navbutton">
+                    <button className="navbutton navbutton_middle">
                         <a className='button_nav_home' href="/real_este">Office for Rent</a>
                     </button>
                     <button className="navbutton">
