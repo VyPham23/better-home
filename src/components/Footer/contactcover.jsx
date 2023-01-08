81da1cf02010ee71698632950e9f20bc6ea51792
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-01-08 14:23:21 +0700

    Feat: completing fix home page

--- src/components/Footer/contactcover.jsx
+++ src/components/Footer/contactcover.jsx
@@ -5,20 +5,16 @@ import './footer.css';
 const ContactCover = () => {
     return (
         <div className="banner_footer d-flex align-items-center" style={{ backgroundImage: `url(${back})` }}>
-            <div className="bg-custom">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 mx-auto">
-                            <div className="banner-area text-center pt-4 pb-4">
-                                <p>We supply the best apartment service for human</p>
-                                <h2 className="contact-title mt-2 mb-4">Need For Apartment Service</h2>
-                            </div>
-                            <div>
-                                <button className="contact-btn">
-                                    <a className='button_nav_home' href="/contact">Contact Us</a>
-                                </button>
-                            </div>
-                        </div>
+            <div className='footer_contact_area'>
+                <div className='w-100 h-100'>
+                    <div className="banner-area text-center pt-4 pb-4">
+                        <p>We supply the best apartment service for human</p>
+                        <h2 className="contact-title mt-2 mb-4">Need For Apartment Service</h2>
+                    </div>
+                    <div>
+                        <button className="contact-btn">
+                            <a className='button_nav_home' href="/contact">Contact Us</a>
+                        </button>
                     </div>
                 </div>
             </div>
