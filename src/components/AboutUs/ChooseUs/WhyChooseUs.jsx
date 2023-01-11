02899c7c8218e42fbd1dbf00bf4501b49038c448
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-01-11 15:18:11 +0700

    Feat: redo about page with smaller scale

--- src/components/AboutUs/ChooseUs/WhyChooseUs.jsx
+++ src/components/AboutUs/ChooseUs/WhyChooseUs.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import './WhyChooseUs.css'
-import NumberChooseUs from './NumberChooseUs';
 import WhyChoose_img1 from './WhyChoose_img1.png'
 
 
 const WhyChooseUs = () => {
-  return (
-    <section className='ChooseUs'>
-        <NumberChooseUs/>
-        <h1 className='title_chooseUs'>
-            Why Choose <span style={{color:'#00908E'}}>Us</span> 
-        </h1>
-        <Row justify="center">
-            <Col span={10} className='list_chooseUs'>
-                <ul>
-                    <li>Experience And Passion About Real Estate</li>
-                    <li>Individual and Creative Approach</li>
-                    <li>The Lastest Technologies </li>
-                </ul>
-            </Col>
-            <Col span={10}>
-                <img src={WhyChoose_img1} alt='WhyChoose_img' className='WhyChoose_img1 img-fluid'></img>
-            </Col>
-        </Row>  
+    return (
+        <section className='ChooseUs'>
+            <h1 className='title_chooseUs'>
+                Why Choose <span style={{ color: '#00908E' }}>Us</span>
+            </h1>
+            <Row justify="center" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                <Col xs={24} sm={10} md={10} lg={10} className='list_chooseUs'>
+                    <div className='block_why_choose_us'>
+                        <li>Experience And Passion About Real Estate</li>
+                        <li>Individual and Creative Approach</li>
+                        <li>The Lastest Technologies </li>
+                    </div>
+                </Col>
+                <Col xs={24} sm={10} md={10} lg={10}>
+                    <img src={WhyChoose_img1} alt='WhyChoose_img' className='WhyChoose_img1 img-fluid'></img>
+                </Col>
+            </Row>
 
 
-    </section>
-  )
+        </section>
+    )
 }
 
 export default WhyChooseUs
