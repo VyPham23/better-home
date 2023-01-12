85e39fecb6dfd264082f8b6acb74609216e5c2ef
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2023-01-12 10:26:08 +0700

    Feat: redo bottom contact page and responsive with smaller scale

--- src/components/Contact/BodyContact.jsx
+++ src/components/Contact/BodyContact.jsx
@@ -8,61 +8,9 @@ import Image from 'react-bootstrap/Image'
 export default function BodyContact() {
   return (
     <div>
-      <Row className='mt-5'></Row>
-      <div className='contact-area'>
-        <Col xs={12} md={1}></Col>
-
-        <div className='row col-lg-10 col-md-12 col-12 contact-block'>
-          <div className='col-lg-5 col-md-6 col-6 box'>
-            <Card className='shadow  bg-gray rounded' style={{ width: '100%' }}>
-              <div className="contact-top">
-                <img className='image-contact-ava' src="./images/ava1.jpg" alt="image-contact" />
-                <div className='contact-name'>Trần Đông Giang</div>
-              </div>
-              <div className='box-contact'>
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/whatsapp.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/zalo.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/talk.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-              </div>
-            </Card>
-          </div>
-
-          <div className='col-lg-5 col-md-6 col-6 box'>
-            <Card className='shadow  bg-gray rounded' style={{ width: '100%' }}>
-              <div className="contact-top">
-                <img className='image-contact-ava' src="./images/ava2.png" alt="image-contact" />
-                <div className='contact-name'>Trần Văn A</div>
-              </div>
-              <div className='box-contact'>
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/whatsapp.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/zalo.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-                <div className='row contact_details'>
-                  <Col><Image className='img1' src="./images/talk.png" class="rounded float-left" alt="..." /></Col>
-                  <Col><p className='text_contact_details'>0905 934 981</p></Col>
-                </div>{/* end row */}
-              </div>
-            </Card>
-          </div>
-        </div>
-
-        <Col xs={12} md={1}></Col>
+      <div className='contact-area mt-3'>
+          
       </div>
-      <Row className='mt-5'></Row>
     </div>
   )
 }
--- src/components/Contact/ButtonsContact.jsx
+++ src/components/Contact/ButtonsContact.jsx
@@ -4,24 +4,24 @@ export default function ButtonsContact() {
   return (
     <div className='buttons_contact mt-5 mb-3'>
       <div>
-        <h2 id='title1'>GET IN TOUCH</h2>
-        <p id='title2' className='text-center'>Anytime, anywhere, with superior quality products, we are with you at every moment</p>
+        <h2 className='title_contact_infor'>GET IN TOUCH</h2>
+        <p className='text-center description_contact'>Anytime, anywhere, with superior quality products, we are with you at every moment</p>
       </div>
-      <div className='row mt-5'>
-        <div className='col-lg-4 col-md-12 col-12 mb-2'>
+      <div className='row mt-3'>
+        <div className='col-lg-4 col-md-4 col-sm-4 col-12 mb-2'>
           <button className="intro_button_2"><i class="bi bi-geo-alt" style={{ fontSize: 20 }}></i>
             <br /> 92C Nguyen Huu Canh St,
             <br /> Binh Thanh District, Ho Chi Minh City
           </button>
         </div>
-        <div className='col-lg-4 col-md-12 col-12 mb-2'>
+        <div className='col-lg-4 col-md-4 col-sm-4 col-12 mb-2'>
           <button className="intro_button_2">
             <i class="bi bi-telephone" style={{ fontSize: 20 }}></i>
             <br />
             <br />(+84) 090 453 234
           </button>
         </div>
-        <div className='col-lg-4 col-md-12 col-12 mb-2'>
+        <div className='col-lg-4 col-md-4 col-sm-4 col-12 mb-2'>
           <button className="intro_button_2">
             <i class="bi bi-envelope-open" style={{ fontSize: 20 }}></i>
             <br />
